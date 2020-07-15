ae6eb1d5f15dde1704e38db74684eb9feab863ee
Max Veytsman <maxim@ontoillogical.com>
2020-07-15 16:17:32 -0400

    Clickable divs!

--- lib/pixel_party_web/live/page_live.ex
+++ lib/pixel_party_web/live/page_live.ex
@@ -3,9 +3,11 @@ defmodule PixelPartyWeb.PageLive do
   @impl true
   def render(assigns) do
     ~L"""
+    <div id="last_clicked">Last clicked: <%= inspect @last_clicked %></div>
       <div id="grid" class="grid">
         <%= for y <- 0..(@height-1), x <- 0..(@height-1) do %>
-          <div id="<%= x %>,<%= y %>" class="grid-cell <%= @grid[{x,y}] %>"></div>
+          <div id="<%= x %>,<%= y %>" class="grid-cell <%= @grid[{x,y}] %>"
+              phx-click="click" phx-value-id="<%= x %>,<%= y %>"></div>
         <% end %>
       </div>
     """
@@ -16,6 +18,22 @@ defmodule PixelPartyWeb.PageLive do
     width = 30
     height = 30
     grid = for x <- 0..(width-1), y <- 0..(height-1), into: %{}, do: {{x,y}, :white}
-    {:ok, assign(socket, width: width, height: height, grid: grid)}
+    {:ok, assign(socket, width: width, height: height, grid: grid, last_clicked: nil, last_color: nil)}
   end
+
+  def handle_event("click", %{"id" => id}, %{assigns: %{grid: grid}}=socket) do
+    id = id |> String.split(",") |> Enum.map(&(String.to_integer(&1))) |> List.to_tuple
+    {:noreply, assign(socket, :last_clicked, id) |> assign(:grid, Map.put(grid, id, next_color(grid[id])))}
+  end
+
+  def next_color(:white), do: :black
+  def next_color(:black), do: :gray
+  def next_color(:gray), do: :red
+  def next_color(:red), do: :orange
+  def next_color(:orange), do: :yellow
+  def next_color(:yellow), do: :green
+  def next_color(:green), do: :blue
+  def next_color(:blue), do: :indigo
+  def next_color(:indigo), do: :violet
+  def next_color(:violet), do: :black
 end
