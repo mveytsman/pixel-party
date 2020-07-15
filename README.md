6976e84dd7b546e2fb9eb537d2029276e9f1eb75
Max Veytsman <maxim@ontoillogical.com>
2020-07-15 14:29:22 -0400

    Draw grid

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 1. Get elixir installed (if using Nix, just run `nix-shell` in this directory)
 2. `mix archive.install hex phx_new 1.5.3`
 3. `mix phx.new . --app pixel_party --module PixelParty --live --no-ecto --no-gettext --no-dashboard`
+4. Bump live view in `mix.exs` -> `{:phoenix_live_view, "~> 0.14.1"},` and run `mix deps.get`
 
 (this is available in the git tag `initial`)
 
--- lib/pixel_party_web/live/page_live.ex
+++ lib/pixel_party_web/live/page_live.ex
@@ -1,15 +1,21 @@
 defmodule PixelPartyWeb.PageLive do
   use PixelPartyWeb, :live_view
-
-
   @impl true
   def render(assigns) do
     ~L"""
-    <p>Hello World</p>
+      <div id="grid" class="grid">
+        <%= for y <- 0..(@height-1), x <- 0..(@height-1) do %>
+          <div id="<%= x %>,<%= y %>" class="grid-cell <%= @grid[{x,y}] %>"></div>
+        <% end %>
+      </div>
     """
   end
+
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    width = 30
+    height = 30
+    grid = for x <- 0..(width-1), y <- 0..(height-1), into: %{}, do: {{x,y}, :white}
+    {:ok, assign(socket, width: width, height: height, grid: grid)}
   end
 end
