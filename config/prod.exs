11906fd534f90a2adaab9a8471f29ca6d013f8cd
Max Veytsman <maxim@ontoillogical.com>
2020-07-15 23:25:35 -0400

    Set up for prod

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,8 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :pixel_party, PixelPartyWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  http: [port: {:system, "PORT"}], # Possibly not needed, but doesn't hurt
+  url: [nil, port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
